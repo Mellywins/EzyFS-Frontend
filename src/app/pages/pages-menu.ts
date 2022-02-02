155df6928c98bb7c2e7b8a4f3c29c3bf672f7b80
Mellywins <zouaghi.wow1919@gmail.com>
2022-02-02 21:57:43 +0100

    text changes, playing around with the dashboard

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,14 +2,14 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'E-commerce',
-    icon: 'shopping-cart-outline',
+    title: 'Home',
+    icon: 'home-outline',
     link: '/pages/dashboard',
     home: true,
   },
   {
-    title: 'IoT Dashboard',
-    icon: 'home-outline',
+    title: 'Jobs',
+    icon: 'settings-outline',
     link: '/pages/iot-dashboard',
   },
   {
